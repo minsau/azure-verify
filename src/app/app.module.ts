e710169f571846dcdd676f86a2e9e5144a97562b
minsau <minsau2@gmail.com>
2017-07-02 23:35:04 -0500

    Agregado servicio para Face API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FaceApiService } from "./services/index.services";
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    FaceApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/index.services.ts
@@ -0,0 +1,1 @@
+export { FaceApiService } from './face-api.service'
